19e37eb842e805410bc0b821500a07db7eaf1a52
Andrew <ondralukes06@seznam.cz>
2023-07-20 12:52:44 +0200

    hero s oběma nastavitelnými headingy

--- components/Hero.js
+++ components/Hero.js
@@ -11,7 +11,8 @@ const Hero = ({ firstHeading, secondHeading, showReadButton }) => {
       </div>
       <div className="absolute top-1/3 right-0 w-150   left-1/16 sm:right-1/2 pr-24 pt-8 pl-8 pb-6 flex flex-col items-start gap-2 bg-gradient-to-br from-primary-color to-white shadow-lg">
         <h1 className="p-0 text-white whitespace-normal leading-relaxed outlined-text">
-          <span dangerouslySetInnerHTML={{ __html: firstHeading }} />
+          {/* <span dangerouslySetInnerHTML={{ __html: firstHeading }} /> */}
+          STUDENTSKÝ <br /> ČASOPIS
         </h1>
         {secondHeading !== '' && (
           <h2 className="leading-tight">
