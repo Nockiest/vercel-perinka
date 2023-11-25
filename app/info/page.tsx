198eb7ded7933b78477945020f2e23325d9aca63
Andrew <ondralukes06@seznam.cz>
2023-11-25 17:07:09 +0100

    prace na strance aktu8lni cislo

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import Hero from '../../components/global/Hero'
+import React from "react";
+import Hero from "../../components/global/Hero";
+import Section from "../../components/global/Section";
+import InfoPost from "./InforPost";
 
 const Informace = () => {
   return (
     <div>
-         <Hero firstHeading={"INFORMACE"} secondHeading={"NEJNOVĚJŠÍ UDÁLOSTI"} />
-    </div>
-  )
-}
+      <Hero firstHeading={"INFORMACE"} secondHeading={"NEJNOVĚJŠÍ UDÁLOSTI"} />
+      <Section classNames={"brown"}>
+        <InfoPost  textContent="První letošní číslo je tady! A podíleli se na něm i noví členové
+          redakce ze tříd 1.J a 2.E. Papírové časopisy opět najdete v celé
+          škole, nově jsme ale do studovny, knihovny a před řiditelnu přidali
+          barevné archivy, ve kterých budou umístěna i starší čísla. Přeji vám
+          krásné počtení a níže samozřejmě najdete stručný přehled toho, co jsme
+          si pro vás připravili ve čtvrtém čísle Peřinky."
+         />
+      </Section>
+      <Section classNames={"bg-stone-300"}>
+      <InfoPost
+          title={'NOVÝ FEED A POČTENÍČKO'}
+          textContent="První letošní číslo je tady! A podíleli se na něm i noví členové
+          redakce ze tříd 1.J a 2.E. Papírové časopisy opět najdete v celé
+          škole, nově jsme ale do studovny, knihovny a před řiditelnu přidali
+          barevné archivy, ve kterých budou umístěna i starší čísla. Přeji vám
+          krásné počtení a níže samozřejmě najdete stručný přehled toho, co jsme
+          si pro vás připravili ve čtvrtém čísle Peřinky."
 
-export default Informace
+          image={{
+            src: '/randomPics/pic1.jpg',
+            position: 'left' ,
+            alt: 'randompic'
+          }}
+         />
+
+      </Section>
+    </div>
+  );
+};
+
+
+
+
+
+
+export default Informace;
--- components/global/Section.tsx
+++ components/global/Section.tsx
@@ -1,24 +1,25 @@
 import React, { ReactNode } from 'react';
 
 interface SectionProps {
-  color: string;
+  color?: string;
   children: ReactNode;
   classNames?: string; // Optional classNames
   style?: React.CSSProperties; // Optional inline styles
 }
 
 const Section: React.FC<SectionProps> = ({ color, children, classNames, style }) => {
-  const sectionStyle: React.CSSProperties = {
-    backgroundColor: color,
-    padding: '20px', // Add your desired padding
-    ...style,
-  };
 
-  return (
-    <section className={classNames} style={sectionStyle}>
-      {children}
-    </section>
-  );
-};
+
+    const sectionStyle: React.CSSProperties = {
+      padding: '2em 8em', // Add your desired padding
+      ...style,
+    };
+
+    return (
+      <section className={classNames} style={sectionStyle}>
+        {children}
+      </section>
+    );
+  };
 
 export default Section;
