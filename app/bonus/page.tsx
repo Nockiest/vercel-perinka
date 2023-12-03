49d8140539224db2b0ed37a3cbd154abeceecb19
Andrew <ondralukes06@seznam.cz>
2023-12-03 16:17:03 +0100

    improved the bonus content chooser

--- app/bonus/page.tsx
+++ app/bonus/page.tsx
@@ -4,6 +4,7 @@ import BigGallery from "../../components/bonuspagecomponents/BigGallerry";
 import BonusPost from "../../components/bonuspagecomponents/BonusPost";
 import Hero from "../../components/global/Hero";
 import   { useState } from "react";
+import PostPreview from "../../components/bonuspagecomponents/BonusArticlePreview";
 
 
 
@@ -12,29 +13,19 @@ import   { useState } from "react";
 const Bonus = () => {
   const links = [
     { href: 'bonus/0', label: 'Lihihvy', image: '/randomPics/pic1.jpg' },
-    { href: 'bonus/1', label: 'Pro Hráče z Opatova', image: '/randop/pic1.jpg' },
-    { href: 'bonus/2', label: 'Lihihvy', image: '/randop/pic1.jpg' },
-    { href: 'bonus/3', label: 'Lihihvy', image: '/randop/pic1.jpg' },
+    { href: 'bonus/1', label: 'Pro Hráče z Opatova', image: '/randomPics/pic1.jpg' },
+    { href: 'bonus/2', label: 'Lihihvy', image: '/randomPics/pic2.jpg' },
+    { href: 'bonus/3', label: 'Lihihvy', image: '/randomPics/pic1.jpg' },
   ];
 
   return (
     <div className=" ">
       <>
         <Hero firstHeading={"BONUSOVÉ ČLÁNKY"} secondHeading={"BONUSOVÉ ČLÁNKY"} />
-        <div className=" h-350 flex items-center justify-evenly">
+        <div className=" flex my-4 gap-4 flex-col items-center justify-evenly">
           {links.map((link, index) => (
-            <div
-              key={index}
-              className="bg-primary-color-80 hover-trigger text-white font-bold rounded-lg p-16 shadow-md flex flex-col items-center group relative overflow-hidden m-2"
-            >
-              <Link  className="  text-lg" href={link.href}>
-                {link.label}
-              </Link>
-              <div className="hovered-content absolute -bottom-16 transition-all duration-300 opacity-0 group-hover:opacity-100">
+            <PostPreview heading={link.label} text={'lorem something Pro Hráče z Opatova lorem something Pro Hráče z Opatova lorem something Pro Hráče z Opatova  something Pro Hráče z Opatova lorem something Pro Hráče z Opatova  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov  something Pro Hráče z Opatova lorem something Pro Hráče z Opatov'} releaseDate={'xxxx:xx:xx'} image={link.image} href={link.href} />
 
-                <img src={link.image} alt={`Hovered Image ${index}`} className="w-16 h-16 rounded-full" />
-              </div>
-            </div>
           ))}
         </div>
       </>
@@ -43,3 +34,17 @@ const Bonus = () => {
 };
 
 export default Bonus;
+
+
+  // <div
+            //   key={index}
+            //   className="bg-primary-color-80 hover-trigger text-white font-bold rounded-lg p-16 shadow-md flex flex-col items-center group relative overflow-hidden m-2"
+            // >
+            //   <Link  className="  text-lg" href={link.href}>
+            //     {link.label}
+            //   </Link>
+            //   <div className="hovered-content absolute -bottom-16 transition-all duration-300 opacity-0 group-hover:opacity-100">
+
+            //     <img src={link.image} alt={`Hovered Image ${index}`} className="w-16 h-16 rounded-full" />
+            //   </div>
+            // </div>
---
+++ components/bonuspagecomponents/BonusArticlePreview.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { storage } from "../../firebase";
+import { getDownloadURL, ref } from "firebase/storage";
+function PostPreview({ heading, text, image, releaseDate, href }) {
+    const postTitle = heading.replace(/\s+/g, "-").toLowerCase();
+    const [imageUrl, setImageUrl] = useState("");
+    const router = useRouter();
+
+    useEffect(() => {
+      const fetchImageUrl = async () => {
+        try {
+          const imageRef = ref(storage, `images/${image}`);
+          const url = await getDownloadURL(imageRef);
+          setImageUrl(url);
+        } catch (error) {
+          console.error("Error fetching image URL:", error);
+        }
+      };
+
+      fetchImageUrl();
+    }, [image]);
+
+    const handlePostClick = () => {
+      router.push(`/${href}`);
+    };
+
+    return (
+      <div className="flex relative flex-col w-2/3 gap-16 p-4 shadow-md">
+        <a href={href}>
+
+
+
+        <div className="flex flex-row h-32 bg-white border-b border-black">
+          <Image src={image} alt={image} width={120} height={240} />
+          <div className="flex flex-col w-full gap-2 p-2">
+            <h4 className="cursor-pointer" onClick={handlePostClick}>
+              {heading}
+            </h4>
+
+            <p className="overflow-hidden   overflow-ellipsis">{text}</p>
+          </div>
+          <p className="  ">{releaseDate}</p>
+         </div>
+        </a>
+      </div>
+    );
+  }
+
+  export default PostPreview;
+
+
+//   {
+// // pathname: `/blog/${postTitle}` ,
+// //   query: { postId: id, slug: heading },
+// }
